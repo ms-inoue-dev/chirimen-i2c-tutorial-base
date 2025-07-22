eb22b3dccc7d7b57ea6436e9d3ebece85437b10f
ms-inoue <ms-inoue@KTEC-2004PC0002.local>
2025-07-22 11:41:06 +0900

    温度、湿度の1秒ごと表示処理に誤りがあったため修正

--- app.mjs
+++ app.mjs
@@ -6,11 +6,15 @@ const i2cPort = i2cAccess.ports.get(1);
 
 const sht40 = new SHT40(i2cPort, 0x44);
 
-await sht40.init();
-
-let data = await sht40.readData();
-
 setInterval(() => {
-    console.dir('湿度：' + data.humidity);
-    console.dir('温度：' + data.temperature);
+    getsht40Data().then(result => {
+        console.dir('湿度：' + result.humidity);
+        console.dir('温度：' + result.temperature);
+    });
 }, 1000);
+
+async function getsht40Data () {
+    await sht40.init();
+    let data = await sht40.readData();
+    return data;
+}
