85e3fd5f3554b96e946059d02e90a6dd7338fbcf
ms-inoue <ms-inoue@KTEC-2004PC0002.local>
2025-07-23 11:05:05 +0900

    方向検出の初期化処理を修正

--- mma7660.js
+++ mma7660.js
@@ -37,19 +37,23 @@ class MMA7660{
   }
   async init(){
     this.i2cSlave = await this.i2cPort.open(this.slaveAddress);
+    setMode(this.i2cSlave, MMA7660_STAND_BY);
+    setSampleRate(this.i2cSlave, AUTO_SLEEP_32);
+    setMode(this.i2cSlave, MMA7660_ACTIVE);
   }
   async getXYZ(){
     if (this.i2cSlave == null) {
       throw new Error("i2cSlave is not open yet.");
     }
 
+    let val = [64, 64, 64];
     let XYZdata = this.i2cSlave.readBytes(3);
 
-    XYZdata[0] = (XYZdata[0] << 2) / 4;
-    XYZdata[1] = (XYZdata[1] << 2) / 4;
-    XYZdata[2] = (XYZdata[2] << 2) / 4;
+    val[0] = (XYZdata[0] << 2) / 4;
+    val[1] = (XYZdata[1] << 2) / 4;
+    val[2] = (XYZdata[2] << 2) / 4;
 
-    return XYZdata;
+    return val;
   }
   async getAcceleration(){
     if (this.i2cSlave == null) {
@@ -60,3 +64,11 @@ class MMA7660{
   }
 }
 export default MMA7660;
+
+function setMode (i2cSlave) {
+  i2cSlave.writeByte(MMA7660_MODE);
+}
+
+function setSampleRate (i2cSlave) {
+  i2cSlave.writeByte(MMA7660_SR);
+}
