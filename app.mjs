7ea9dba9be2bfe01f54b3f3caa2a91e4e096c7c6
ms-inoue-dev <m.inoue.0802@gmail.com>
2025-07-31 11:04:00 +0900

    水位センサーのapp.mjsの動作確認

--- app.mjs
+++ app.mjs
@@ -1,28 +1,25 @@
 import {requestI2CAccess} from "chirimen";
-import MMA7660 from "@chirimen/mma7660";
+import WaterLevelSensor from "@chirimen/grove-water-level-sensor";
 
 async function main() {
-
     const i2cAccess = await requestI2CAccess();
 
     const i2cPort = i2cAccess.ports.get(1);
 
-    const mma7660 = new MMA7660(i2cPort, 0x4c);
+    const waterlevelsensor = new WaterLevelSensor(i2cPort, 0x77, 0x78);
 
-    await mma7660.init();
+    await waterlevelsensor.init();
 
     setInterval(async() => {
-        const XYZData = await mma7660.getXYZ();
-        const AccelerationData = await mma7660.getAcceleration();
+        const high12SectionValue = await waterlevelsensor.getHigh12SectionValue();
+        const low8SectionValue = await waterlevelsensor.getLow8SectionValue();
 
-        console.dir(`X =${  XYZData.X}`);
-        console.dir(`y =${  XYZData.Y}`);
-        console.dir(`z =${  XYZData.Z}`);
+        const waterLevel = await waterlevelsensor.getWaterLevel();
 
-        console.dir(`accleration of X/Y/Z: ${  AccelerationData.X  } g/ ${  AccelerationData.Y  } g/ ${  AccelerationData.Z  } g`);
-
+        console.dir(`high: ${  high12SectionValue}`);
+        console.dir(`low: ${  low8SectionValue}`);
+        console.dir(`waterLevel: ${  waterLevel  } %`);
     }, 500);
-
 }
 
 main();
